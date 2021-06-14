1817aa4ac93a51231a955dc45cab5d2c942101ef
hunghh.dev <hunghh.dev@gmail.com>
2021-06-14 10:42:33 +0700

    Book selector

--- src/component/book/BookContainer.jsx
+++ src/component/book/BookContainer.jsx
@@ -1,13 +1,18 @@
 import { Box } from '@material-ui/core';
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import getBooksAction from '../../module/book/bookAction';
+import { getBooksSelector } from '../../module/book/bookSelector';
 import BookFilter from './BookFilter';
 import styles from './BookStyles';
 
 const BookContainer = () => {
     const dispatch = useDispatch();
-    dispatch(getBooksAction());
+    useEffect(() => {
+        dispatch(getBooksAction());
+    }, [dispatch])
+    const books = useSelector(getBooksSelector);
+    console.log(books)
     const classes = styles();
     return (
         <Box className={classes.bookContainer}>
