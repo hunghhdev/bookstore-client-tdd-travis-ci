37eda1a9007eb2446aef818a7527c5a50cffb3a3
hunghh.dev <hunghh.dev@gmail.com>
2021-06-14 09:49:51 +0700

    redux setup

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,21 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux'
+import reduxThunk from 'redux-thunk'
 import App from './component/App'
+import reducers from './module'
 
-ReactDom.render(<App />, document.getElementById('root'))
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+
+ReactDom.render(
+    <Provider store={
+        createStoreWithMiddleware(
+            reducers,
+            window.__REDUX_DEVTOOLS_EXTENSION__
+            && window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
+    }>
+        <App />
+    </Provider>,
+    document.getElementById('root'))
